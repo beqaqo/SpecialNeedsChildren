94de398baacc68a9563dff4a2c6ac6ddab3fd845
beqaqo <beka.kopadze.2@iliauni.edu.ge>
2026-04-28 20:20:37 +0400

    changes in api

--- src/endpoints/round_api.py
+++ src/endpoints/round_api.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-from src.models.round import Round
+from src.models import Round, Letter
 from src.ext import api
 
 ns_rounds = Namespace("rounds", path = '/api')
@@ -8,18 +8,27 @@ rounds_model = api.model(
     "Rounds",
     {"id": fields.Integer, "position": fields.Integer, "image": fields.String,})
 
-words_model = api.model("Words", {
+letter_model = api.model("Letter", {
+    "id": fields.Integer,
+    "letter": fields.String,
+    "image": fields.String,
+    "sound": fields.String,
+})
+
+word_with_letters_model = api.model("WordWithLetters", {
     "id": fields.Integer,
     "word": fields.String,
     "image": fields.String,
     "sound": fields.String,
-    "round_id": fields.Integer,})
+    "round_id": fields.Integer,
+    "letters": fields.List(fields.Nested(letter_model))
+})
 
 
 round_with_words_model = api.model("WordWithLetters", {
     "id": fields.Integer,
     "image": fields.String,
-    "words": fields.List(fields.Nested(words_model))
+    "words": fields.List(fields.Nested(word_with_letters_model)),
 })
 
 @ns_rounds.route("/rounds")
@@ -27,6 +36,13 @@ class RoundListAPI(Resource):
     @ns_rounds.marshal_with(rounds_model)
     def get(self):
         rounds = Round.query.all()
+        for round in rounds:
+            for word in round.words:
+                letters = [
+                    Letter.query.filter(Letter.letter == l).first()
+                    for l in word.word
+                ]
+                word.letters = letters
         return rounds
 
 @ns_rounds.route("/rounds/<int:id>")
@@ -34,4 +50,11 @@ class RoundAPI(Resource):
     @ns_rounds.marshal_with(round_with_words_model)
     def get(self, id):
         round = Round.query.get_or_404(id)
+        for word in round.words:
+            letters = [
+                    Letter.query.filter(Letter.letter == l).first()
+                    for l in word.word
+                ]
+            word.letters = letters
+
         return round
--- src/endpoints/word_api.py
+++ src/endpoints/word_api.py
@@ -1,25 +1,9 @@
 from flask_restx import Namespace, Resource, fields
 
-from src import Letter
-from src.models.word import Word
+from src.models import Word, Letter
 from src.ext import api
 
 ns_words = Namespace("words", path="/api")
-
-words_model = api.model("Words", {
-    "id": fields.Integer,
-    "word": fields.String,
-    "image": fields.String,
-    "sound": fields.String,
-    "round_id": fields.Integer,})
-
-
-@ns_words.route("/words")
-class WordListAPI(Resource):
-    @ns_words.marshal_with(words_model)
-    def get(self):
-        words = Word.query.all()
-        return words
 
 letter_model = api.model("Letter", {
     "id": fields.Integer,
@@ -37,14 +21,16 @@ word_with_letters_model = api.model("WordWithLetters", {
     "letters": fields.List(fields.Nested(letter_model))
 })
 
-@ns_words.route("/words/<int:id>")
-class WordAPI(Resource):
+
+@ns_words.route("/words")
+class WordListAPI(Resource):
     @ns_words.marshal_with(word_with_letters_model)
-    def get(self, id):
-        word = Word.query.get(id)
-        letters = [
-            Letter.query.filter(Letter.letter == l).first()
-            for l in word.word
-        ]
-        word.letters = letters
-        return word
+    def get(self):
+        words = Word.query.all()
+        for word in words:
+            letters = [
+                Letter.query.filter(Letter.letter == l).first()
+                for l in word.word
+            ]
+            word.letters = letters
+        return words
