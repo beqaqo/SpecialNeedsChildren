4ba4ef357b9898b486e2d495e03ff519dc20bfab
beqaqo <beka.kopadze.2@iliauni.edu.ge>
2026-04-28 18:00:24 +0400

    just test

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from src.ext import db, migrate, login_manager, admin, api
 from src.config import Config
 from src.commands import init_db, populate_db
-from src.models import Word, Round, Letter, User  # noqa
+from src.models import Word, Round, Letter, User
 from src.admin_views.base import SecureModelView, SecureIndexView
 from src.views.auth import auth_blueprint
 from src.endpoints import ns_rounds, ns_words
@@ -13,6 +13,38 @@ from src.admin_views.letter_view import LetterView
 COMMANDS = [init_db, populate_db]
 BLUEPRINTS = [auth_blueprint]
 
+def create_app():
+    # app = Flask(__name__)
+    # app.config.from_object(Config)
+    try:
+        print("Creating app...")
+
+        app = Flask(__name__)
+        app.config.from_object(Config)
+
+        print("Config loaded")
+
+        register_extensions(app)
+        print("Extensions registered")
+
+        register_blueprints(app)
+        print("Blueprints registered")
+
+        register_commands(app)
+        print("Commands registered")
+
+        return app
+
+    except Exception as e:
+        print("🔥 APP CRASHED:", e)
+        import traceback
+        traceback.print_exc()
+        raise
+
+    #register_extensions(app)
+    #register_blueprints(app)
+    #register_commands(app)
+    #return app
 
 def register_extensions(app):
     db.init_app(app)
@@ -43,12 +75,3 @@ def register_blueprints(app):
 def register_commands(app):
     for command in COMMANDS:
         app.cli.add_command(command)
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    register_extensions(app)
-    register_blueprints(app)
-    register_commands(app)
-    return app
